c1fc4c43a2fd60b8888f2c591a37473aff5d2895
fjcx0000 <fjcx0000@gmail.com>
2017-02-19 23:07:19 +1100

    First commit

---
+++ app/Models/Brand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    public $incrementing = false;
+    public $primaryKey = 'brand_id';
+}
