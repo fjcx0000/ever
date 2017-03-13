3e2a19c688d8a5ae913a93dbc60960079a08b406
fjcx0000 <fjcx0000@gmail.com>
2017-03-13 23:23:17 +1100

    sku finished 2

---
+++ app/Http/Controllers/EbayController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: think
+ * Date: 13/03/2017
+ * Time: 11:13 PM
+ */
+
+namespace App\Http\Controllers;
+
+use Config;
+use Dinero;
+use Datatables;
+use Illuminate\Http\Request;
+
+class EbayController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function index(Request $request)
+    {
+
+    }
+
+}
